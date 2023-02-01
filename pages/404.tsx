582c8d39d14dcaa114739397fe31624ef8a4f0ce
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2023-02-01 04:04:17 -0700

    Create 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,77 @@
+import { useRef } from 'react';
+import Head from 'next/head';
+
+import styles from '@/styles/Home.module.css';
+import Footer from '@/components/layout/footer';
+import Navbar from '@/components/layout/navbar';
+import SiteHeader from '@/components/layout/site-header';
+
+export default function Intheory() {
+  const portfolio = useRef<HTMLDivElement>(null);
+  const team = useRef<HTMLDivElement>(null);
+  const contact = useRef<HTMLDivElement>(null);
+
+  const portfolioScroll = () => {
+    if (portfolio.current) {
+      portfolio.current.scrollIntoView({ block: 'center' });
+    }
+  };
+
+  const teamScroll = () => {
+    if (team.current) {
+      team.current.scrollIntoView({ block: 'start' });
+    }
+  };
+
+  const contactScroll = () => {
+    if (contact.current) {
+      contact.current.scrollIntoView({ block: 'start' });
+    }
+  };
+
+  return (
+    <>
+      <SiteHeader />
+      <main className={styles.main}>
+        <Navbar
+          portfolioScroll={portfolioScroll}
+          teamScroll={teamScroll}
+          contactScroll={contactScroll}
+        />
+        <div
+          style={{
+            height: '100vh',
+            width: '100vw',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <div style={{ textAlign: 'center' }}>
+            <h3 style={{ fontSize: '2rem', lineHeight: '4rem' }}>
+              404... page not found 😵‍💫
+            </h3>
+            <p style={{ lineHeight: '2rem', marginBottom: '20px' }}>
+              Were you looking for inTheory?
+              <br />
+              <a
+                style={{ textDecoration: 'underline', filter: 'opacity(0.6)' }}
+                href="https://www.intheory.science">
+                https://www.intheory.science
+              </a>
+            </p>
+            <p style={{ lineHeight: '2rem', marginBottom: '20px' }}>
+              ... or the IF home page?
+              <br />
+              <a
+                style={{ textDecoration: 'underline', filter: 'opacity(0.6)' }}
+                href="https://www.impact-finance.io">
+                https://www.impact-finance.io
+              </a>
+            </p>
+          </div>
+        </div>
+        <Footer />
+      </main>
+    </>
+  );
+}
