2cb728677d11cba312a7939773c75110d352cc87
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2023-03-13 03:22:32 -0600

    fixing animations

--- components/about/focus-item.tsx
+++ components/about/focus-item.tsx
@@ -1,5 +1,7 @@
 import Image, { StaticImageData } from 'next/image';
-import { Fade } from 'react-awesome-reveal';
+// import { Fade } from 'react-awesome-reveal';
+import { Waypoint } from 'react-waypoint';
+import { useState } from 'react';
 
 import styles from './focus-item.module.css';
 
@@ -18,62 +20,102 @@ const FocusItem = ({
   description,
   shape,
 }: FocusItemProps) => {
+  const [boxInView, setBoxInView] = useState(false);
+  const [textInView, setTextInView] = useState(false);
   return (
     <>
       {justify === 0 ? (
-        <div className={styles.itemBox}>
-          <Image
-            src={image}
-            alt={title}
-            height={350}
-            width={600}
-            className={styles.isoRight}
-          />
-          <Image
-            src={shape}
-            alt="floating shape"
-            height={160}
-            width={160}
-            className={styles.shapeRight}
-          />
-          <div className={styles.textRight}>
-            <Fade
-              duration={750}
-              triggerOnce
-              direction="right"
-              cascade>
-              <h3 className={styles.title}>{title}</h3>
-              <p className={styles.description}>{description}</p>
-            </Fade>
+        <Waypoint
+          onEnter={() => {
+            setBoxInView(true);
+          }}>
+          <div
+            className={
+              boxInView ? styles.itemBox : `${styles.itemBox} ${styles.offDown}`
+            }>
+            <Image
+              src={image}
+              alt={title}
+              height={350}
+              width={600}
+              className={styles.isoRight}
+            />
+            <Image
+              src={shape}
+              alt="floating shape"
+              height={160}
+              width={160}
+              className={styles.shapeRight}
+            />
+            <Waypoint
+              onEnter={() => {
+                setTextInView(true);
+              }}
+              bottomOffset={150}>
+              <div
+                className={
+                  textInView
+                    ? styles.textRight
+                    : `${styles.textRight} ${styles.transparent}`
+                }>
+                {/* <Fade
+            duration={750}
+            triggerOnce
+            direction="right"
+            cascade> */}
+                <h3 className={styles.title}>{title}</h3>
+                <p className={styles.description}>{description}</p>
+                {/* </Fade> */}
+              </div>
+            </Waypoint>
           </div>
-        </div>
+        </Waypoint>
       ) : (
-        <div className={styles.itemBox}>
-          <div className={styles.textLeft}>
-            <Fade
+        <Waypoint
+          onEnter={() => {
+            setBoxInView(true);
+          }}>
+          <div
+            className={
+              boxInView ? styles.itemBox : `${styles.itemBox} ${styles.offDown}`
+            }>
+            <Waypoint
+              onEnter={() => {
+                setTextInView(true);
+              }}
+              bottomOffset={150}>
+              <div
+                className={
+                  textInView
+                    ? styles.textLeft
+                    : `${styles.textLeft} ${styles.transparent}`
+                }>
+                {/* <Fade
               duration={750}
               triggerOnce
               direction="left"
-              cascade>
-              <h3 className={styles.title}>{title}</h3>
-              <p className={styles.description}>{description}</p>
-            </Fade>
+              cascade> */}
+                <h3 className={styles.title}>{title}</h3>
+                <p className={styles.description}>{description}</p>
+                {/* </Fade> */}
+              </div>
+            </Waypoint>
+            <Image
+              src={image}
+              alt={title}
+              height={350}
+              width={600}
+              className={styles.isoLeft}
+            />
+            <Image
+              src={shape}
+              alt="floating shape"
+              height={160}
+              width={160}
+              className={styles.shapeLeft}
+            />
           </div>
-          <Image
-            src={image}
-            alt={title}
-            height={350}
-            width={600}
-            className={styles.isoLeft}
-          />
-          <Image
-            src={shape}
-            alt="floating shape"
-            height={160}
-            width={160}
-            className={styles.shapeLeft}
-          />
-        </div>
+        </Waypoint>
       )}
     </>
   );
