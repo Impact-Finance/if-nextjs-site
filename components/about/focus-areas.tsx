012d4d7508431100a347506b1de25db02fd8e38b
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2023-03-13 11:09:32 -0600

    animation smoothing

--- components/about/focus-areas.tsx
+++ components/about/focus-areas.tsx
@@ -1,5 +1,4 @@
-// import { Fade } from 'react-awesome-reveal';
-import { Waypoint } from 'react-waypoint';
+import { Fade } from 'react-awesome-reveal';
 
 import FocusItem from './focus-item';
 import desci from '@/public/isometrics/desci-iso.png';
@@ -13,40 +12,40 @@ import styles from './focus-areas.module.css';
 const FocusAreas = () => {
   return (
     <div className={styles.focusContainer}>
-      {/* <Fade
+      <Fade
         duration={1000}
         triggerOnce
-        direction="down"> */}
-      <h2 className={styles.headline}>Designing solutions for...</h2>
-      {/* </Fade> */}
+        direction="down">
+        <h2 className={styles.headline}>Designing solutions for...</h2>
+      </Fade>
       <div className={styles.items}>
-        {/* <Fade
+        <Fade
           duration={1000}
           triggerOnce
           direction="up"
-          cascade> */}
-        <FocusItem
-          title="Decentralized Science"
-          image={desci}
-          justify={0}
-          description="Transparent and open-access scientific research, governed and funded by informed communities."
-          shape={cube}
-        />
-        <FocusItem
-          title="Sustainable Public Goods"
-          image={pg}
-          justify={1}
-          description="Non-extractive and incentive-aligned systems with the power to support vital community resources."
-          shape={triangle}
-        />
-        <FocusItem
-          title="Climate Action x Web3"
-          image={climate}
-          justify={0}
-          description="Targeted and verifiable contributions towards saving our planet, powered by human networks."
-          shape={pipe}
-        />
-        {/* </Fade> */}
+          cascade>
+          <FocusItem
+            title="Decentralized Science"
+            image={desci}
+            justify={0}
+            description="Transparent and open-access scientific research, governed and funded by informed communities."
+            shape={cube}
+          />
+          <FocusItem
+            title="Sustainable Public Goods"
+            image={pg}
+            justify={1}
+            description="Non-extractive and incentive-aligned systems with the power to support vital community resources."
+            shape={triangle}
+          />
+          <FocusItem
+            title="Climate Action x Web3"
+            image={climate}
+            justify={0}
+            description="Targeted and verifiable contributions towards saving our planet, powered by human networks."
+            shape={pipe}
+          />
+        </Fade>
       </div>
     </div>
   );
--- components/about/focus-item.tsx
+++ components/about/focus-item.tsx
@@ -1,7 +1,5 @@
 import Image, { StaticImageData } from 'next/image';
-// import { Fade } from 'react-awesome-reveal';
-import { Waypoint } from 'react-waypoint';
-import { useState } from 'react';
+import { Fade } from 'react-awesome-reveal';
 
 import styles from './focus-item.module.css';
 
@@ -20,102 +18,62 @@ const FocusItem = ({
   description,
   shape,
 }: FocusItemProps) => {
-  const [boxInView, setBoxInView] = useState(false);
-  const [textInView, setTextInView] = useState(false);
   return (
     <>
       {justify === 0 ? (
-        <Waypoint
-          onEnter={() => {
-            setBoxInView(true);
-          }}>
-          <div
-            className={
-              boxInView ? styles.itemBox : `${styles.itemBox} ${styles.offDown}`
-            }>
-            <Image
-              src={image}
-              alt={title}
-              height={350}
-              width={600}
-              className={styles.isoRight}
-            />
-            <Image
-              src={shape}
-              alt="floating shape"
-              height={160}
-              width={160}
-              className={styles.shapeRight}
-            />
-            <Waypoint
-              onEnter={() => {
-                setTextInView(true);
-              }}
-              bottomOffset={150}>
-              <div
-                className={
-                  textInView
-                    ? styles.textRight
-                    : `${styles.textRight} ${styles.transparent}`
-                }>
-                {/* <Fade
-            duration={750}
-            triggerOnce
-            direction="right"
-            cascade> */}
-                <h3 className={styles.title}>{title}</h3>
-                <p className={styles.description}>{description}</p>
-                {/* </Fade> */}
-              </div>
-            </Waypoint>
+        <div className={styles.itemBox}>
+          <Image
+            src={image}
+            alt={title}
+            height={350}
+            width={600}
+            className={styles.isoRight}
+          />
+          <Image
+            src={shape}
+            alt="floating shape"
+            height={160}
+            width={160}
+            className={styles.shapeRight}
+          />
+          <div className={styles.textRight}>
+            <Fade
+              duration={750}
+              triggerOnce
+              direction="right"
+              cascade>
+              <h3 className={styles.title}>{title}</h3>
+              <p className={styles.description}>{description}</p>
+            </Fade>
           </div>
-        </Waypoint>
+        </div>
       ) : (
-        <Waypoint
-          onEnter={() => {
-            setBoxInView(true);
-          }}>
-          <div
-            className={
-              boxInView ? styles.itemBox : `${styles.itemBox} ${styles.offDown}`
-            }>
-            <Waypoint
-              onEnter={() => {
-                setTextInView(true);
-              }}
-              bottomOffset={150}>
-              <div
-                className={
-                  textInView
-                    ? styles.textLeft
-                    : `${styles.textLeft} ${styles.transparent}`
-                }>
-                {/* <Fade
+        <div className={styles.itemBox}>
+          <div className={styles.textLeft}>
+            <Fade
               duration={750}
               triggerOnce
               direction="left"
-              cascade> */}
-                <h3 className={styles.title}>{title}</h3>
-                <p className={styles.description}>{description}</p>
-                {/* </Fade> */}
-              </div>
-            </Waypoint>
-            <Image
-              src={image}
-              alt={title}
-              height={350}
-              width={600}
-              className={styles.isoLeft}
-            />
-            <Image
-              src={shape}
-              alt="floating shape"
-              height={160}
-              width={160}
-              className={styles.shapeLeft}
-            />
+              cascade>
+              <h3 className={styles.title}>{title}</h3>
+              <p className={styles.description}>{description}</p>
+            </Fade>
           </div>
-        </Waypoint>
+          <Image
+            src={image}
+            alt={title}
+            height={350}
+            width={600}
+            className={styles.isoLeft}
+          />
+          <Image
+            src={shape}
+            alt="floating shape"
+            height={160}
+            width={160}
+            className={styles.shapeLeft}
+          />
+        </div>
       )}
     </>
   );
